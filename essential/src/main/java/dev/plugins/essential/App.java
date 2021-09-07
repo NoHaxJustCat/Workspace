a7b09176406140169c3977e196b9b1e6cb96be8a
BearToCode <davidebasso03@gmail.com>
2021-09-07 19:23:52 +0200

    hello

--- essential/src/main/java/dev/plugins/essential/App.java
+++ essential/src/main/java/dev/plugins/essential/App.java
@@ -1,11 +1,14 @@
 package dev.plugins.essential;
 
+import dev.plugins.essential.commands.HelloCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
+
+        new HelloCommand(this);
     }
 
     @Override
