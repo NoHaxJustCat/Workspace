a7bdc8088e58b3fde2c392c82261513b6f59438c
BearToCode <davidebasso03@gmail.com>
2021-09-07 17:27:24 +0200

    ciao

--- essential/src/main/java/dev/plugins/essential/App.java
+++ essential/src/main/java/dev/plugins/essential/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Plugin initialized!" );
+        System.out.println( "Plugin initialized! Viva i gatti!" );
     }
 }
