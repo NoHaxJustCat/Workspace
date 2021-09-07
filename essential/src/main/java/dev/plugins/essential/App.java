1b5f86dcdf8364ad7e800abb17e7a71482f9984b
NoHaxJustCat <78616044+NoHaxJustCat@users.noreply.github.com>
2021-09-07 16:09:17 +0200

    base del plugin

---
+++ essential/src/main/java/dev/plugins/essential/App.java
@@ -0,0 +1,13 @@
+package dev.plugins.essential;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
